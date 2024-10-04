c4cb459ed95e7d5adf0421d33f7d45e53f3879b8
kms0233 <cnblue789@naver.com>
2024-10-04 21:23:34 +0900

    test1

--- GitPrac/GitPrac/ViewController.swift
+++ GitPrac/GitPrac/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello! This is Test 1 for Git")
         // Do any additional setup after loading the view.
     }
 
