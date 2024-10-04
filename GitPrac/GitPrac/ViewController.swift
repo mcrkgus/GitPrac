04545246c10d58759169f5b3fb9a7fb3c4b911fc
mcrkgus <mcrkgus@naver.com>
2024-10-04 21:21:35 +0900

    Setting

---
+++ GitPrac/GitPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPrac
+//
+//  Created by Gahyun Kim on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
